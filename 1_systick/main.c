4e23dfa5ada1cfca6c90db32265b57ab8fc6fce3
EasonDowYo <king0906sk@gmail.com>
2019-06-19 14:45:14 +0800

    Homework

--- 1_systick/main.c
+++ 1_systick/main.c
@@ -6,23 +6,22 @@ void setup_systick(uint32_t ticks);
 
 int main(void)
 {
-	setup_systick(??????); //200 ms
+	setup_systick(168e6/8/5); //200 ms
 	blink(LED_BLUE); //should not return
 }
 
 void setup_systick(uint32_t ticks)
 {
 	// set reload value
-	??????
-
+    WRITE_BITS(SYST_BASE + SYST_RVR_OFFSET ,23,0,ticks-1  );
 	// uses external reference clock
-	??????
+	
 
 	// enable systick exception
-	??????
+	
 
 	// enable systick
-	??????
+	REG(SYST_BASE +SYST_CSR_OFFSET )=3;
 }
 
 void systick_handler(void)
